2ba74577017504d35aded235feb728804856bb0b
ilkoTheTiger <ilkothetiger@gmail.com>
2023-11-05 00:57:14 +0200

    Revert "Attempted to implement SOAP Service"
    This reverts commit 631d3adf82d45ae1d80a096f1243de2914b391f5.

--- MyWebApp/Program.cs
+++ MyWebApp/Program.cs
@@ -1,12 +1,7 @@
-using SoapCore;
-using SoapInNetCore.BusinessLogic;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddSoapCore();
-builder.Services.AddScoped<ISoapService, SoapService>();
 
 var app = builder.Build();
 
@@ -20,11 +15,6 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.UseSoapEndpoint<ISoapService>("/Service.asmx", new SoapEncoderOptions(), SoapSerializer.XmlSerializer);
-});
 
 app.MapRazorPages();
 
